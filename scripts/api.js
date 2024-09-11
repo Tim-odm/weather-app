be1c25f3d2af2e39e291022f27b8f1ee9474653d
Tim-odm <t.makoni.tm16@gmail.com>
2024-09-11 15:50:14 +0100

    make request and get data methods

---
+++ scripts/api.js
@@ -0,0 +1,23 @@
+// This file makes the API requests.
+
+async function makeRequest(location) {
+  const data = fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=HRTUZPM6L2TNX2ZC9XJJPL7EB&contentType=json`, {mode: "cors"})
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      return data;
+    })
+    .catch(function(err) {
+      console.log("There was an error");
+    })
+   return data; 
+}
+
+async function getData() {
+  const data = await makeRequest("swansea");
+  console.log(data)
+}
+
+getData();
