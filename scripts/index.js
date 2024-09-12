9266df970ff251d28f594c8fe8cb2008cf354e13
Tim-odm <t.makoni.tm16@gmail.com>
2024-09-12 17:07:39 +0100

    update current conditions

--- scripts/index.js
+++ scripts/index.js
@@ -6,4 +6,14 @@ searchSubmitBtn.addEventListener("click", async (e) => {
   const location = document.getElementById("location-search").value;
   const data = await api.makeRequest(location);
   console.log(data);
+  updateCurrentConditions();
 })
+
+// Update the current conditions section
+async function updateCurrentConditions() {
+  const currentCoditions = document.querySelectorAll("#current-conditions > p");
+  const data = await api.getCurrentConditionsData();
+  Object.keys(data).forEach((i, index) => {
+    currentCoditions[index].innerText = data[i];
+  })
+}
