462a161abf72f6cc4185fd32d663029fca83833f
Tim-odm <t.makoni.tm16@gmail.com>
2024-09-12 16:29:37 +0100

    event listener for form

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
-import "./api.js";
+import * as api from "./api.js";
+
+const searchSubmitBtn = document.querySelector("input[type=submit]");
+searchSubmitBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+  const location = document.getElementById("location-search").value;
+  const data = await api.makeRequest(location);
+  console.log(data);
+})
