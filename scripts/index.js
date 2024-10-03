4039ac308895aa3b4e858495dc230d2376532e71
Tim-odm <t.makoni.tm16@gmail.com>
2024-10-03 14:30:50 +0100

    update upcoming days html

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,7 @@ searchSubmitBtn.addEventListener("click", async (e) => {
   const data = await api.makeRequest(location);
   console.log(data);
   updateCurrentConditions(location);
+  updateUpcomingDays(location);
 })
 
 // Update the current conditions section
@@ -15,5 +16,30 @@ async function updateCurrentConditions(location) {
   const data = await api.getCurrentConditionsData(location);
   Object.keys(data).forEach((i, index) => {
     currentCoditions[index].innerText = data[i];
+  })
+}
+
+async function updateUpcomingDays(location) {
+  const tableBody = document.getElementById("upcoming-days-table");
+  tableBody.innerHTML = "";
+  const data = await api.getUpcomingDaysData(location);
+  
+  data.forEach((day) => {
+    // create row
+    // fill innerhtml on row
+    // add row to table body
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+    <td>${day.date}</td>
+    <td>${day.conditions}</td>
+    <td>${day.tempmax}&degC</td>
+    <td>${day.tempmin}&degC</td>
+    <td>${day.sunrise}pm</td>
+    <td>${day.sunset}am</td>
+    <td>${day.cloudcover}</td>
+    <td>${day.humidity}%</td>
+    `
+    tableBody.appendChild(row);
   })
 }
